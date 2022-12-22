71bd789d94703dbba359bf5d1952c53432e65eb7
Prosefionol <ilya_kuper@mail.ru>
2022-12-22 19:11:11 +0300

    Add BottomNavigationBar

---
+++ app/src/main/java/com/vk/fitnesstracker/Progress.kt
@@ -0,0 +1,31 @@
+package com.vk.fitnesstracker
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Progress : Fragment() {
+
+    companion object {
+        fun newInstance() = Progress()
+    }
+
+    private lateinit var viewModel: ProgressViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_progress, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProgressViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
